8408712c7e9ca4f2260487566a34d581e716daff
GuilleDev <ocana.alborta.guillermo@gmail.com>
2025-05-28 14:33:59 -0400

    Add Footer component with copyright notice and gradient background

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="w-screen bg-black flex justify-center items-center">
-      <footer className="text-center p-4 text-white">
-      Todos los derechos reservados © {new Date().getFullYear()} Parcon Publicidad Digital e impresa.
-    </footer>
-    </div>
-  );
-}
